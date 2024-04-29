4393a0816a18c17ee8abaf52ef31fc17742975e9
akjha-17 <amankumarj17@gmail.com>
2024-04-29 18:38:48 +0530

    detailsModification

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AppointmentDetailsService } from '../../services/appointment-details.service';
 import { NgFor, NgIf } from '@angular/common';
-
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [NgIf,NgFor],
+  imports: [NgIf,NgFor,DatePipe],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
@@ -13,7 +13,7 @@ export class DetailsComponent implements OnInit {
   appointment: any | undefined;
   constructor(private appointmentDetailsService: AppointmentDetailsService) {}
   ngOnInit(): void {
-   this.appointmentDetailsService.GetAppointmentDetail(5)
+   this.appointmentDetailsService.GetAppointmentDetail(6)
    .subscribe((appointment: any) => this.appointment = appointment);
   }
 
