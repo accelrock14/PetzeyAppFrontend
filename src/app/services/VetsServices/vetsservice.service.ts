b0f06fe0408201c3e2e6ee8d24ec298b00c1a719
Soujanya-S6 <soujanya6620@gmail.com>
2024-04-29 23:55:48 +0530

    added Vet Service

--- src/app/services/VetsServices/vetsservice.service.ts
+++ src/app/services/VetsServices/vetsservice.service.ts
@@ -1,28 +1,50 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 import { IPet } from '../../models/Pets/IPet';
+import { IVet } from '../../models/Vets/IVet';
+import { IVetIdNameDTO } from '../../models/Vets/IVetIdNameDTO';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VetsserviceService {
 
-  constructor(private http:HttpClient) { }
+  private apiUrl = 'https://localhost:44304/api/vets';
 
+  constructor(private http: HttpClient) { }
 
-  getHighRatedVets():Observable<IPet>{
-
-    const url=""
-    return this.http.get<IPet>(url)
-
+  getAllVets(): Observable<IVet[]> {
+    return this.http.get<IVet[]>(this.apiUrl);
   }
 
+  getVetById(id: number): Observable<IVet> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.get<IVet>(url);
+  }
 
-  getAllVets():Observable<IPet[]>{
-    const url=""
-    return this.http.get<IPet[]>(url);
+  addVet(vet: IVet): Observable<IVet> {
+    return this.http.post<any>(this.apiUrl, vet);
+  }
 
+  updateVet(id: number, vet: IVet): Observable<IVet> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.put<IVet>(url, vet);
+  }
+
+  deleteVet(id: number): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<IVet>(url);
+  }
+
+  editStatus(id: number, status: boolean): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.patch<any>(url, status);
+  }
+
+  getVetsByListOfIds(doctorIds: number[]): Observable<IVetIdNameDTO[]> {
+    const url = `${this.apiUrl}/VetDetails`;
+    return this.http.post<IVetIdNameDTO[]>(url, doctorIds);
   }
 
   
