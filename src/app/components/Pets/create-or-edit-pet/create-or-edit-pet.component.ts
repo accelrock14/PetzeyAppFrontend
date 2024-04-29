128e1e3f7e662feec113ebb0d1b0ffad6d48a308
Shashank Joshi <sj.shashankjoshi.sj@gmail.com>
2024-04-30 00:34:39 +0530

    fix-edit and create component

--- src/app/components/Pets/create-or-edit-pet/create-or-edit-pet.component.ts
+++ src/app/components/Pets/create-or-edit-pet/create-or-edit-pet.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, input } from '@angular/core';
+import { IPet } from '../../../models/Pets/IPet';
+import { ActivatedRoute } from '@angular/router';
+import { PetsService, petToken } from '../../../services/PetsServices/pets.service';
+import { I18nPluralPipe } from '@angular/common';
 
 @Component({
   selector: 'app-create-or-edit-pet',
@@ -8,6 +12,23 @@ import { Component } from '@angular/core';
   styleUrl: './create-or-edit-pet.component.css'
 })
 export class CreateOrEditPetComponent {
+  constructor(private activatedRoute:ActivatedRoute, private petService: PetsService){
+
+  }
+
+@Input()
+ToBeUpdatedPet?:IPet;
+
+Pet?:IPet;
+
+
+SaveUpdatedPetDetails() {
+  if (this.ToBeUpdatedPet) {
+    this.petService.EditPet(this.ToBeUpdatedPet).subscribe(temp =>
+      this.Pet = temp
+    );
+  }
+}
 
 }
 
