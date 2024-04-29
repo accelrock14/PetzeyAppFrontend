10c6b9fe7d9704cb604c803f36f2d3f555e25d9f
Mih1rGupt4 <mihirgupta422@gmail.com>
2024-04-29 20:34:58 +0530

    created appointment pets profile component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AppointmentPetProfileComponent } from './components/Pets/appointment-pet-profile/appointment-pet-profile.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    { path: '', component: HomeComponent },
+    { path: 'appointmentPetDetails', component:AppointmentPetProfileComponent }
 ];
---
+++ src/app/components/Pets/appointment-pet-profile/appointment-pet-profile.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input } from '@angular/core';
+import { IPet } from '../../../models/Pets/IPet';
+
+@Component({
+  selector: 'app-appointment-pet-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './appointment-pet-profile.component.html',
+  styleUrl: './appointment-pet-profile.component.css'
+})
+export class AppointmentPetProfileComponent {
+
+  @Input()
+  Pet:IPet = {
+    PetID: 0,
+    PetParentId: 5,
+    PetName: 'Doggo',
+    PetImage: 'arraybuffer',
+    Species: 'Dog',
+    Breed: 'Afghan Hound',
+    Gender: 'Female',
+    DateOfBirth: new Date(),
+    Age: 4,
+    Allergies: '',
+    LastAppointmentdate: ''
+  }
+
+  Owner = {
+    ownerName: "Jack Hall",
+    email: "jhondoe@gmail.com",
+    address: "Bangalore, Karnataka, 560102",
+    phoneNo : "+44 2071838750"
+  }
+}
