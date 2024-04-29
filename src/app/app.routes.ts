50b5d02b1fb0e3c137adf64614c2bff6eb39893a
Shashank Joshi <sj.shashankjoshi.sj@gmail.com>
2024-04-29 15:52:49 +0530

    added createoreditmodalview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { CreateOrEditPetComponent } from './components/Pets/create-or-edit-pet/create-or-edit-pet.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent }
+    {
+        path: '', component: HomeComponent
+    },
+    {
+        path:'createandedittemp', component:CreateOrEditPetComponent
+    }
 ];
