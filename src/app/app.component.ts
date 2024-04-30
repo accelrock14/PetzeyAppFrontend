697d28324c1bee30a0337b6f0779baf5e6bb6231
Soujanya-S6 <soujanya6620@gmail.com>
2024-04-30 09:11:44 +0530

    vet component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { VetComponent } from './components/Vet/vet/vet.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet,RouterLink,VetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/Vet/vet/vet.component.ts
+++ src/app/components/Vet/vet/vet.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
+import { IVetCardDTO } from '../../../models/Vets/IVetCardDto';
+import { VetsserviceService } from '../../../services/VetsServices/vetsservice.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-vet',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './vet.component.html',
   styleUrl: './vet.component.css'
 })
 export class VetComponent {
 
-  
+  vets: IVetCardDTO[] = [];
+
+  constructor(private vetService: VetsserviceService) { }
+
+  ngOnInit(): void {
+    this.getAllVets();
+  }
+
+  getAllVets(): void {
+    this.vetService.getAllVets()
+      .subscribe(
+        (vets: IVetCardDTO[]) => {
+          this.vets = vets;
+          console.log(this.vets)
+        },
+        error => {
+          console.error('Error fetching vets:', error);
+        }
+      );
+  }
 
 
 
