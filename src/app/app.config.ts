cec720a7842c55bddb4c212e40f9261e54f5eeab
Mih1rGupt4 <mihirgupta422@gmail.com>
2024-04-30 22:16:41 +0530

    corrected imports

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,8 +5,6 @@ import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { DashboardService } from './services/DashboardServices/dashboard.service';
 
-import { provideHttpClient } from '@angular/common/http';
-
 
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
