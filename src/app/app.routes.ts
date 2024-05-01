0e401162564755f8bad8adedc2606047bb302eec
akjha-17 <amankumarj17@gmail.com>
2024-05-01 10:30:17 +0530

    appointment changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { DetailsComponent } from './components/details/details.component';
 import { ReportComponent } from './components/appointments/report/report.component';
 import { ReportHistoryComponent } from './components/appointments/report-history/report-history.component';
 import { AdminDashboardComponent } from './components/dashboard/admin-dashboard/admin-dashboard.component';
