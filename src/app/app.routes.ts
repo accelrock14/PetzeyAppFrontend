684f4b479e383e260ac09f6470f07e46eacf70ed
Mihir Gupta <93673193+Mih1rGupt4@users.noreply.github.com>
2024-05-01 12:32:20 +0530

    Pets branch (#9)
    * added createoreditmodalview
    
    * installed bootstrap icons
    
    * created appointment pets profile component
    
    * edit pet added
    
    * pets list grid
    
    * pets list grid
    
    * seperated create and edit modal views
    
    * shared folder for storing api urls for services
    
    * add service functionality for filtering pets with pageination
    
    * fix-edit and create component
    
    * enable pagination feature to pets list grid
    
    * pagination bug fixes
    
    * add age pip and remove age property
    
    * add age pip and remove age property
    
    * bug fix commit
    
    * create-pet fix with age removal
    
    * change lastappointmentdate property data type
    
    * update property name
    
    * recently consulted
    
    * pet profile
    
    * file conversion temp
    
    * tempfile commit
    
    * ..
    
    * added user profile
    
    * corrected imports
    
    * ng multiselect error
    
    * ..
    
    * ..
    
    * added and modified component for pagination
    
    * bug fixes
    
    * reactive form update for edit pet
    
    ---------
    
    Co-authored-by: Shashank Joshi <sj.shashankjoshi.sj@gmail.com>
    Co-authored-by: Ankita S <Ankita S@DESKTOP-9A8QKLN>
    Co-authored-by: Viru2219 <vik20ec@cmrit.ac.in>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,31 +11,50 @@ import { PatientDashboardComponent } from './components/dashboard/patient-dashbo
 import { AppointmentPetProfileComponent } from './components/Pets/appointment-pet-profile/appointment-pet-profile.component';
 import { CreateOrEditPetComponent } from './components/Pets/create-or-edit-pet/create-or-edit-pet.component';
 import { PetsListGridComponent } from './components/Pets/pets-list-grid/pets-list-grid.component';
-import { PetsListGridWithPagesComponent } from './components/Pets/pets-list-grid-with-page/pets-list-grid-with-page.component';
 import { UserProfileComponent } from './components/Pets/user-profile/user-profile.component';
 import { PetProfileComponent } from './components/Pets/pet-profile/pet-profile.component';
+import { PetsListGridPagedComponent } from './components/Pets/pets-list-grid-paged/pets-list-grid-paged.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'details', component: DetailsComponent},
+  { path: '', component: HomeComponent },
+  { path: 'details', component: DetailsComponent },
   { path: 'report', component: ReportComponent },
   { path: 'reporthistory', component: ReportHistoryComponent },
-    { path: "AdminDashboard", component: AdminDashboardComponent, title: "AdminDashboard" },
-    { path: "DoctorDashboard", component: DoctorDashboardComponent, title: "DoctorDashboard" },
-    { path: 'PatientDashboard', component: PatientDashboardComponent, title: "PatientDashboard" },
-    {
-        path:'createandedittemp', component:CreateOrEditPetComponent
-    },
+  {
+    path: 'AdminDashboard',
+    component: AdminDashboardComponent,
+    title: 'AdminDashboard',
+  },
+  {
+    path: 'DoctorDashboard',
+    component: DoctorDashboardComponent,
+    title: 'DoctorDashboard',
+  },
+  {
+    path: 'PatientDashboard',
+    component: PatientDashboardComponent,
+    title: 'PatientDashboard',
+  },
+  {
+    path: 'createandedittemp',
+    component: CreateOrEditPetComponent,
+  },
 
-    {
-      path:'pets-list', component:PetsListGridComponent
-    },
-    {
-      path:'pets-profile/:id', component:PetProfileComponent
-    },
-    { path: 'pets/:page', component: PetsListGridWithPagesComponent }, // Route for the component with page number parameter
+  {
+    path: 'pets-list',
+    component: PetsListGridComponent,
+  },
+  {
+    path: 'pets-profile/:id',
+    component: PetProfileComponent,
+  },
+  { path: 'pets/:page', component: PetsListGridPagedComponent }, // Route for the component with page number parameter
 
-    { path: 'appointmentPetDetails', component:AppointmentPetProfileComponent },
+  { path: 'appointmentPetDetails', component: AppointmentPetProfileComponent },
 
-    { path: 'user', component: UserProfileComponent}
+  { path: 'user', component: UserProfileComponent },
+  {
+    path: 'editpet/:id',
+    component: CreateOrEditPetComponent,
+  },
 ];
