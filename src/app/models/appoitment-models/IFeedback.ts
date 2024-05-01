abc478d2b8fd8e80a030a112c57579b802920a9e
salman <2002syeds@gmail.com>
2024-05-01 17:37:45 +0530

    feedback changes and merged main

---
+++ src/app/models/appoitment-models/IFeedback.ts
@@ -0,0 +1,33 @@
+// export interface FeedBack{
+//  FeedbackID:number
+//  Competence:number,
+//  Outcome:number,
+//  Booking:number,
+//  Recommendation:'Yes' | 'No'|'PreferNotToSay',
+//  Comments:string
+//     AppointmentId:number,
+// }
+
+export interface Feedback {
+    FeedbackID: number;
+    Questions: Question[];
+    Recommendation: string;
+    Comments: string;
+    AppointmentId: number;
+}
+export interface Question {
+    QuestionId: number;
+    FeedbackQuestionId: number;
+    Rating: number;
+}
+// export interface Ratings{
+//     Competence:number,
+//     Outcome:number,
+//     Booking:number,
+//     Recommendation:'Yes' | 'No'|'PreferNotToSay',
+//     Comments:string
+// }
+export interface FeedbackQuestion{
+    FeedbackQuestionId:number;
+    FeedbackQuestionName:string;
+}
