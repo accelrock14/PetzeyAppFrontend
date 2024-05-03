e3cb1d2db190870acf646c35ea6713cb7b33a552
Mih1rGupt4 <mihirgupta422@gmail.com>
2024-05-03 14:39:42 +0530

    modify user profile in appointment profile

--- src/app/components/Pets/appointment-pet-profile/appointment-pet-profile.component.ts
+++ src/app/components/Pets/appointment-pet-profile/appointment-pet-profile.component.ts
@@ -2,18 +2,24 @@ import { Component, Input, OnInit } from '@angular/core';
 import { IPet } from '../../../models/Pets/IPet';
 import { AgePipe } from "../../../pipes/Age/age.pipe";
 import { PetsService } from '../../../services/PetsServices/pets.service';
+import { AuthService } from '../../../services/UserAuthServices/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-appointment-pet-profile',
     standalone: true,
     templateUrl: './appointment-pet-profile.component.html',
     styleUrl: './appointment-pet-profile.component.css',
-    imports: [AgePipe]
+    imports: [AgePipe, CommonModule]
 })
 export class AppointmentPetProfileComponent implements OnInit {
-  
-  constructor(private petsService: PetsService,) {}
+  Owner: any | null;
+  constructor(private petsService: PetsService,public auth: AuthService) {}
   ngOnInit(): void {
+    if (this.auth.isLoggedIn()) {
+      this.Owner = this.auth.getLoggedInUserObject();
+      console.log(this.Owner);
+    }
     this.petsService.GetPetDetailsByID(this.PetId).subscribe( pet => {
       this.Pet = pet;
       console.log(this.Pet)
@@ -39,11 +45,4 @@ export class AppointmentPetProfileComponent implements OnInit {
     Neutered: false
   }
 
-
-  Owner = {
-    ownerName: "Jack Hall",
-    email: "jhondoe@gmail.com",
-    address: "Bangalore, Karnataka, 560102",
-    phoneNo : "+44 2071838750"
-  }
 }
