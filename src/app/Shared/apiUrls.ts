60b773f3b4a0faf2fd5ffdc496c16606970e4bed
Sudeep M Arashingodi <96487736+Sudeep-code-it@users.noreply.github.com>
2024-05-03 10:47:30 +0530

    added the appointment url

--- src/app/Shared/apiUrls.ts
+++ src/app/Shared/apiUrls.ts
@@ -1,3 +1,5 @@
 // Add required base urls here for easy access
-export const petsServiceUrl = "https://localhost:44374/api/pets"
+export const petsServiceUrl = 'https://localhost:44374/api/pets';
 
+export const appointmentServiceUrl =
+  'https://petzeybackendappointmentapi20240502214622.azurewebsites.net/';
