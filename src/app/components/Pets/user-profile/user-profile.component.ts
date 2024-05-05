6a0adff0283b2d943c4ffd53f83ef8ef75387ffb
Shashank Joshi <sj.shashankjoshi.sj@gmail.com>
2024-05-05 16:19:56 +0530

    edit fix

--- src/app/components/Pets/user-profile/user-profile.component.ts
+++ src/app/components/Pets/user-profile/user-profile.component.ts
@@ -19,6 +19,7 @@ import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 export class UserProfileComponent implements OnInit {
 
 
+
   petParentID:any;
   NewPet?: IPet;
   newPetForm: FormGroup;
@@ -186,6 +187,10 @@ export class UserProfileComponent implements OnInit {
         ...this.petDetailsForm.value
       };
       this.SaveUpdatedPetDetails()
+      this.petsService.GetPetsByParentID(`${this.petParentID}`).subscribe((data) => {
+        this.pets = data;
+    });
+      
     }
   }
   handleFileAdd(event: any): void {
@@ -263,15 +268,15 @@ export class UserProfileComponent implements OnInit {
           next: updatedPet => {
             // Handle success, if needed
             console.log('Pet updated successfully:', updatedPet);
-
+           
 
         },
         error: error => {
             // Handle error, if needed
             console.error('Error updating pet:', error);
         }
+        
       });
-
     }
 
     clickedCancelEditButton() {
@@ -279,6 +284,7 @@ export class UserProfileComponent implements OnInit {
         this.pets = data;
     });
   }
+  
 }
 
 
