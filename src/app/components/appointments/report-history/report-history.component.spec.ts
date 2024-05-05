97cacdfdd3b2830868a74c28fc645a55cbfe192c
Sudeep M Arashingodi <96487736+Sudeep-code-it@users.noreply.github.com>
2024-05-06 03:46:40 +0530

    edited report-history

--- src/app/components/appointments/report-history/report-history.component.spec.ts
+++ src/app/components/appointments/report-history/report-history.component.spec.ts
@@ -1,17 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReportHistoryComponent } from './report-history.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-describe('ReportHistoryComponent', () => {
+fdescribe('ReportHistoryComponent', () => {
   let component: ReportHistoryComponent;
   let fixture: ComponentFixture<ReportHistoryComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReportHistoryComponent]
-    })
-    .compileComponents();
-    
+      imports: [
+        ReportHistoryComponent,
+        HttpClientTestingModule,
+        ToastrModule.forRoot(),
+        BrowserAnimationsModule,
+      ],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(ReportHistoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
