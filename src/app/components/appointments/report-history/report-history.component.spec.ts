99fd1bb0f054766849494f401db2181075a68bd3
Jeferson X <86565887+accelrock14@users.noreply.github.com>
2024-05-06 09:12:39 +0530

    fixed report history test

--- src/app/components/appointments/report-history/report-history.component.spec.ts
+++ src/app/components/appointments/report-history/report-history.component.spec.ts
@@ -1,17 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReportHistoryComponent } from './report-history.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
-describe('ReportHistoryComponent', () => {
+fdescribe('ReportHistoryComponent', () => {
   let component: ReportHistoryComponent;
   let fixture: ComponentFixture<ReportHistoryComponent>;
+  let mockhttp: HttpTestingController
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ReportHistoryComponent]
+      imports: [ReportHistoryComponent, HttpClientTestingModule],
+      providers: provideAnimations()
     })
-    .compileComponents();
-    
+      .compileComponents();
+    mockhttp = TestBed.inject(HttpTestingController)
     fixture = TestBed.createComponent(ReportHistoryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
