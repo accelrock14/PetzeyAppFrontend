8e9e20b001b14e5ecba5203cf73f446315f8b73d
HARSH221202 <harshpratapsingh4151@gmail.com>
2024-05-10 19:03:03 +0530

    added upcoming appointments to the doctor dashboard

--- src/app/components/dashboard/doctor-dashboard/doctor-dashboard.component.ts
+++ src/app/components/dashboard/doctor-dashboard/doctor-dashboard.component.ts
@@ -12,6 +12,7 @@ import { AuthService } from '../../../services/UserAuthServices/auth.service';
 import { VetsserviceService } from '../../../services/VetsServices/vetsservice.service';
 import { IVet } from '../../../models/Vets/IVet';
 import { IDFiltersDto } from '../../../models/Dashboard/IDFiltersDto';
+import { IVetIdNameDTO } from '../../../models/Vets/IVetIDNameDto';
 
 @Component({
   selector: 'app-doctor-dashboard',
@@ -45,6 +46,10 @@ export class DoctorDashboardComponent implements OnInit {
     DoctorID: '',
     OwnerID: ''
   };
+  UpcomingappointmentCards: AppointmentCardDto[] = [];
+  docids : number[] = [];
+  ownerids : string[] = [];
+  docdetails : IVetIdNameDTO[] = [];
 
   constructor(private service: DashboardService, public authService: AuthService, private vetService: VetsserviceService) { }
   ngOnInit(): void {
@@ -65,7 +70,32 @@ export class DoctorDashboardComponent implements OnInit {
       this.service.GetStatusCounts(this.ids).subscribe(count => {
         this.appointmentStatus = count;
       })
-     
+      this.service.GetUpcomingAppointments(this.ids).subscribe(data => {
+        this.UpcomingappointmentCards = data;
+        this.UpcomingappointmentCards.forEach(element => {
+          this.docids.push(parseInt(element.DoctorID));
+        });
+        this.vetService.getVetsByListOfIds(this.docids).subscribe(data => {
+          this.docdetails = data;
+          console.log("data ", data);
+          console.log("docdetails" + this.docdetails[0].Specialization)
+          // Loop through upcoming appointment cards to assign doctor details
+          this.UpcomingappointmentCards.forEach((element) => {
+            const matchingVet = this.docdetails.find(vet => vet.VetId === parseInt(element.DoctorID));
+            if (matchingVet) {
+              element.DoctorName = matchingVet.Name;
+              element.VetSpecialization = matchingVet.Specialization;
+              element.DoctorPhoto = matchingVet.Photo;
+            }
+            else{
+              element.DoctorName = "unknown"
+              element.VetSpecialization = "unknown"
+              element.DoctorPhoto = "unknown"
+            }
+          });
+        })
+      })
+      
     })
   }
 
