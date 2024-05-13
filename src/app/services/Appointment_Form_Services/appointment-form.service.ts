9a1e3995ba7ebe2a67498c944d97611d8644dde2
AMOGH PATIL <AMOGH PATIL@DESKTOP-PC8N705>
2024-05-13 10:36:33 +0530

    added getNoOfOpenAppointmentsOfVet

--- src/app/services/Appointment_Form_Services/appointment-form.service.ts
+++ src/app/services/Appointment_Form_Services/appointment-form.service.ts
@@ -10,6 +10,7 @@ import { IVetCardDTO } from '../../models/Vets/IVetCardDto';
 import { IVet } from '../../models/Vets/IVet';
 import { catchError, map } from 'rxjs/operators';
 import { appointmentServiceUrl } from '../../Shared/apiUrls';
+import { Status } from '../../models/Status';
 
 
 @Injectable({
@@ -82,5 +83,34 @@ export class AppointmentFormService {
   getAllAppointments():Observable<AppointmentDetail[]>{
     return this.backendClient.get<AppointmentDetail[]>(appointmentServiceUrl+"/api/Appointment");
   }
+
+  // getNoOfOpenAppointmentsOfVet(vetID:string):number{
+  //   let appointments:AppointmentDetail[] = [];
+  //     this.getAllAppointments().subscribe({
+  //     next:(data)=>{
+  //       appointments=data;
+  //     },
+  //     error:(err)=>{
+  //       console.log(err);
+  //     }
+  //   });
+  //   return appointments.filter(a=>a.DoctorID==vetID && a.Status== Status.Confirmed).length;
+  // }
+
+  async getNoOfOpenAppointmentsOfVet(vetID: string): Promise<number> {
+    let appointments: AppointmentDetail[] = [];
+    try {
+        const fetchedAppointments = await this.getAllAppointments().toPromise();
+        if (fetchedAppointments) {
+            appointments = fetchedAppointments;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+    return appointments.filter(a => a.DoctorID == vetID && a.Status == Status.Confirmed).length;
+}
+
+
+
 
 }
